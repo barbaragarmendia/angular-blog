40a0123ac57d41f23833384cb30ae361a2c0ede1
Bárbara Garmendia <barbaragarmendia@hotmail.com>
2023-10-14 17:30:55 +0100

    feat: modificando para blog tech

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Cientistas Criam Computador Quântico Portátil Capaz de Resolver Problemas Complexos em Segundos",
+    "description": "Em uma descoberta revolucionária, cientistas desenvolveram um computador quântico portátil que promete transformar a computação como a conhecemos.",
+    "photoCover":"https://via.placeholder.com/400x200"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Inteligência Artificial Revoluciona a Indústria da Saúde: Diagnósticos Precisos em Tempo Recorde",
+    "description": "m uma descoberta revolucionária, cientistas desenvolveram um computador quântico portátil que promete transformar a computação como a conhecemos.",
+    "photoCover":"https://via.placeholder.com/400x200"
   }
 ]
